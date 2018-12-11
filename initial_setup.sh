e2f2dafd256cb1616ab0d64a4750ab24c99a17eb
bren9233 <brennon.thomas@rackspace.com>
2018-12-11 14:42:45 -0600

    Fixed typo

--- initial_setup.sh
+++ initial_setup.sh
@@ -8,7 +8,7 @@ fi
 
 # Ensure pip3 is installed.
 # https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
-command -v pip3 >/dev/null 2>&1 || { echo >&2 "Script require pip3 but it is not installed. \
+command -v pip3 >/dev/null 2>&1 || { echo >&2 "Script requires pip3 but it is not installed. \
 Courtesy pastable: 'sudo apt update && sudo apt install python3-pip -y'"; exit 1; }
 
 # Install Ansible with pip.
