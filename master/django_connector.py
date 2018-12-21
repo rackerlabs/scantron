8ec0f197729f748ca96c25d8127dc9edcc477de4
bren9233 <brennon.thomas@rackspace.com>
2018-12-21 14:45:46 -0600

    tweaked master/django_connector.py

--- master/django_connector.py
+++ master/django_connector.py
@@ -8,4 +8,9 @@ project_path = "."
 sys.path.append(project_path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 django.setup()
-from django_scantron.models import Scan, ScheduledScan  # noqa
+# fmt: off
+from django_scantron.models import (  # noqa
+    Scan,
+    ScheduledScan,
+)
+# fmt: on
