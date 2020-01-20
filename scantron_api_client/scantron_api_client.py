eb1e4876906bf7bf83bf353850ab86becfab79c9
derpadoo <thomab310@gmail.com>
2020-01-20 12:44:13 -0600

    Added scantron_api_client files

---
+++ scantron_api_client/scantron_api_client.py
@@ -0,0 +1,359 @@
+# Standard Python libraries.
+import json
+import sys
+import urllib3
+
+# Third party Python libraries.
+import requests
+
+# Custom Python libraries.
+import utility
+
+__version__ = "1.0"
+
+
+class ScantronClient:
+    def __init__(self, **kwargs):
+        """Initialize a Scantron client."""
+
+        secrets_file_location = "./scantron_api_secrets.json"
+
+        SECRETS = {}
+
+        try:
+            with open(secrets_file_location) as config_file:
+                SECRETS = json.loads(config_file.read())
+        except OSError:
+            print(f"Error: {secrets_file_location} does not exist.  Exiting...")
+            sys.exit(1)
+
+        # Ensure key/values exist in secrets.json.
+        try:
+            self.host = SECRETS["host"]
+            self.port = SECRETS["port"]
+            self.token = SECRETS["token"]
+
+        except KeyError:
+            print(f"Error reading key-values in {secrets_file_location} file.  Exiting...")
+            sys.exit(1)
+
+        # Build BASE_URL.
+        self.BASE_URL = f"https://{self.host}:{self.port}"
+
+        # Minimize Python requests (and the underlying urllib3 library) logging level.
+        # logging.getLogger("requests").setLevel(print)
+        # logging.getLogger("urllib3").setLevel(print)
+
+        self.headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Token {self.token}",
+        }
+
+        # Extract timeout, default to 30 seconds.
+        self.timeout = kwargs.get("timeout", 30)
+
+        # Extract self_signed, default to True.
+        self.api_self_signed = kwargs.get("api_self_signed", True)
+
+        # Extract max attempts, default to 3.
+        self.max_attempts = kwargs.get("max_attempts", 3)
+
+        if self.api_self_signed:
+            urllib3.disable_warnings()
+
+    def scantron_api_query(self, endpoint, **kwargs):
+        """Executes a properly formatted API call to the Scantron API with the supplied arguments."""
+
+        url = f"{self.BASE_URL}/{endpoint}"
+
+        # Set HTTP headers.
+        headers = kwargs.get("headers", {})
+
+        default_headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Token {self.token}",
+        }
+
+        if not isinstance(headers, dict):
+            raise ValueError("headers keyword passed to scantron_api_query is not a valid dict object")
+
+        # Merge dictionaries.
+        # https://treyhunner.com/2016/02/how-to-merge-dictionaries-in-python/
+        headers = {**default_headers, **headers}
+
+        # Extract HTTP verb, defaults to GET.
+        method = kwargs.get("method", "GET")
+        method = method.upper()
+
+        # Extract additional parameters, defaults to an empty dictionary.
+        parameters = kwargs.get("params", {})
+
+        if not isinstance(parameters, dict):
+            raise ValueError("params keyword passed to scantron_api_query is not a valid dict object")
+
+        # Extract payload.
+        payload = kwargs.get("payload", "{}")
+
+        # Used to track number of failed HTTP requests.
+        attempts = 0
+
+        while True:
+            try:
+                if method == "GET":
+                    response = requests.get(
+                        url,
+                        headers=headers,
+                        params=parameters,
+                        json=payload,
+                        verify=(not self.api_self_signed),
+                        timeout=self.timeout,
+                    )
+
+                    if response.status_code != 200:
+                        utility.debug_requests_response(response, endpoint, payload, parameters)
+
+                    break
+
+                elif method == "POST":
+                    response = requests.post(
+                        url,
+                        headers=headers,
+                        params=parameters,
+                        json=payload,
+                        verify=(not self.api_self_signed),
+                        timeout=self.timeout,
+                    )
+
+                    if response.status_code != 201:
+                        utility.debug_requests_response(response, endpoint, payload, parameters)
+
+                    break
+
+                elif method == "PATCH":
+                    response = requests.patch(
+                        url,
+                        headers=headers,
+                        params=parameters,
+                        json=payload,
+                        verify=(not self.api_self_signed),
+                        timeout=self.timeout,
+                    )
+
+                    if response.status_code != 200:
+                        utility.debug_requests_response(response, endpoint, payload, parameters)
+
+                    break
+
+                elif method == "PUT":
+                    response = requests.put(
+                        url,
+                        headers=headers,
+                        params=parameters,
+                        json=payload,
+                        verify=(not self.api_self_signed),
+                        timeout=self.timeout,
+                    )
+
+                    if response.status_code != 200:
+                        utility.debug_requests_response(response, endpoint, payload, parameters)
+
+                    break
+
+                elif method == "DELETE":
+                    response = requests.delete(
+                        url,
+                        headers=headers,
+                        params=parameters,
+                        json=payload,
+                        verify=(not self.api_self_signed),
+                        timeout=self.timeout,
+                    )
+
+                    if response.status_code != 204:
+                        utility.debug_requests_response(response, endpoint, payload, parameters)
+
+                    break
+
+                else:
+                    print(f"Invalid HTTP method passed to scantron_api_query: {method}")
+                    raise ValueError(f"Invalid HTTP method passed to scantron_api_query: {method}")
+
+            except (
+                requests.exceptions.ConnectTimeout,
+                requests.exceptions.ReadTimeout,
+                requests.exceptions.ConnectionError,
+            ):
+                attempts += 1
+                if self.max_attempts < attempts:
+                    print(
+                        f"Unable to reach Scantron API after {self.max_attempts} tries.  Consider increasing the timeout."
+                    )
+                    sys.exit(1)
+                else:
+                    print("Packet loss when attempting to reach the Scantron API.")
+
+        return response
+
+    # Scan Results
+    ##############
+    def retrieve_scan_results(self, scan_id, file_type, **kwargs):
+        """Returns a text blob of the scan results.  For .json files, you can convert it to a Python JSON object using:
+        scan_results_json = json.loads(scan_results)"""
+
+        if file_type.lower() not in ["nmap", "xml", "json"]:
+            print(f"Not a valid file type: {file_type}")
+            scan_results = None
+
+        else:
+            scan_results = self.scantron_api_query(f"/results/{scan_id}?file_type={file_type}", **kwargs).text
+
+        return scan_results
+
+    # AGENTS
+    ########
+    # Agents - CRUD functions.
+    def create_agent(self):
+        """Create a scan command."""
+        print("no create function, an Agent is created when a Django User is created.")
+        return None
+
+    def retrieve_agent(self, agent_id):
+        """Retrieve an agent."""
+        return self.scantron_api_query(f"/api/agents/{agent_id}", method="GET")
+
+    def update_agent(self, agent_id, payload):
+        """Update an agent."""
+        return self.scantron_api_query(f"/api/agents/{agent_id}", method="PATCH", payload=payload)
+
+    def delete_agent(self, agent_id):
+        """Delete an agent."""
+        return self.scantron_api_query(f"/api/agents/{agent_id}", method="DELETE")
+
+    # Agents - Miscellaneous functions.
+    def retrieve_agents(self, **kwargs):
+        """Retrieve information for all the agents."""
+        return self.scantron_api_query("/api/agents").json()
+
+    def retrieve_agent_id_from_agent_name(self, agent_name):
+        """Retrieve the agent ID, given an agent name."""
+
+        agents = self.retrieve_agents()
+
+        agent_id = None
+
+        for agent in agents:
+            if agent["scan_agent"].lower() == agent_name.lower():
+                agent_id = agent["id"]
+
+        return agent_id
+
+    # Scan Commands
+    ###############
+    # Scan Commands - CRUD functions.
+    def create_scan_command(self, payload):
+        """Create a scan command"""
+        return self.scantron_api_query("/api/scan_commands", method="POST", payload=payload)
+
+    def retrieve_scan_command(self, scan_command_id, **kwargs):
+        """Retrieve scan command."""
+        return self.scantron_api_query(f"/api/scan_commands/{scan_command_id}", method="GET").json()
+
+    def update_scan_command(self, scan_command_id, payload):
+        """Update scan command for specific scan command ID"""
+        return self.scantron_api_query(f"/api/scan_commands/{scan_command_id}", method="PATCH", payload=payload)
+
+    def delete_scan_command(self, scan_command_id):
+        """Delete a scan"""
+        return self.scantron_api_query(f"/api/scan_commands/{scan_command_id}", method="DELETE")
+
+    # Scan Commands - Miscellaneous functions.
+    def retrieve_scan_commands(self, **kwargs):
+        """Retrieve information for all the scan commands."""
+        return self.scantron_api_query("/api/scan_commands").json()
+
+    def retrieve_scan_command_id_from_scan_command_name(self, scan_command_name):
+        """Retrieve the scan command ID, given a scan command name."""
+
+        scan_commands = self.retrieve_scan_commands()
+
+        scan_command_id = None
+
+        for scan_command in scan_commands:
+            if scan_command["scan_command_name"].lower() == scan_command_name.lower():
+                scan_command_id = scan_command["id"]
+
+        return scan_command_id
+
+    # SCANS
+    #######
+    # Scans - CRUD functions.
+    def create_scan(self, payload):
+        """Create a scan"""
+        return self.scantron_api_query("/api/scans", method="POST", payload=payload)
+
+    def retrieve_scan(self, scan_id):
+        """Retrieve a scan."""
+        return self.scantron_api_query(f"/api/scans/{scan_id}", method="GET")
+
+    def update_scan(self, scan_id, payload):
+        """Update scan for specific scan ID"""
+        return self.scantron_api_query(f"/api/scans/{scan_id}", method="PATCH", payload=payload)
+
+    def delete_scan(self, scan_id):
+        """Delete a scan"""
+        return self.scantron_api_query(f"/api/scans/{scan_id}", method="DELETE")
+
+    # Scans - Miscellaneous functions.
+    def retrieve_scans(self, **kwargs):
+        """Retrieve information for all the scans."""
+        return self.scantron_api_query("/api/scans").json()
+
+    # SCHEDULED SCANS
+    #################
+    def retrieve_scheduled_scans(self, **kwargs):
+        """Retrieve information for all scheduled scans."""
+        return self.scantron_api_query("/api/scheduled_scans").json()
+
+    # SITES
+    #######
+    # Sites - CRUD functions.
+    def create_site(self, payload):
+        """Create a site."""
+        return self.scantron_api_query("/api/sites", method="POST", payload=payload)
+
+    def retrieve_site(self, site_id):
+        """Retrieve a site."""
+        return self.scantron_api_query(f"/api/sites/{site_id}", method="GET")
+
+    def update_site(self, site_id, payload):
+        """Update a site."""
+        return self.scantron_api_query(f"/api/sites/{site_id}", method="PATCH", payload=payload)
+
+    def delete_site(self, site_id):
+        """Delete a site"""
+        return self.scantron_api_query(f"/api/sites/{site_id}", method="DELETE")
+
+    # Sites - Miscellaneous functions.
+    def retrieve_sites(self, **kwargs):
+        """Retrieve information for all the sites."""
+        return self.scantron_api_query(f"/api/sites").json()
+
+    def retrieve_site_id_from_site_name(self, site_name):
+        """Retrieve the site ID, given a site name."""
+
+        sites_info = self.retrieve_sites()
+
+        site_id = None
+
+        for site in sites_info:
+            if site["site_name"].lower() == site_name.lower():
+                site_id = site["id"]
+
+        return site_id
+
+
+if __name__ == "__main__":
+    print("Use 'import scantron_api_client', do not run directly.")
+    exit(1)
---
+++ scantron_api_client/utility.py
@@ -0,0 +1,161 @@
+"""
+Various utility methods.
+"""
+# Standard Python libraries.
+import ipaddress
+import pprint
+import time
+
+# Third party Python libraries.
+
+# Custom Python libraries.
+
+
+def debug_requests_response(response, endpoint="", payload="", parameters=""):
+    """Provide debug print info for a requests response object."""
+
+    print(f"HTTP server returned error HTTP {response.status_code} - {http_status_code(response.status_code)}")
+    print(f"ENDPOINT: {endpoint}")
+    print(f"PAYLOAD: {payload}")
+    print(f"PARAMETERS: {parameters}")
+    print("HEADERS:")
+    print(pprint.pformat(response.headers))
+    print("RESPONSE:")
+    print(pprint.pformat(response.text))
+
+
+def get_timestamp():
+    """Generates a timestamp."""
+
+    now = time.localtime()
+    timestamp = time.strftime("%Y%m%d_%H%M%S", now)
+
+    return timestamp
+
+
+def get_iso_8601_timestamp_no_second():
+    """Generates an ISO 8601 standardized timestamp.
+    https://en.wikipedia.org/wiki/ISO_8601"""
+
+    now = time.localtime()
+    timestamp = time.strftime("%Y-%m-%dT%H:%M", now)
+
+    return timestamp
+
+
+def expand_range_of_ips(start_ip, end_ip):
+    """Takes an IP range and returns all the IPs in that range.
+    # http://cmikavac.net/2011/09/11/how-to-generate-an-ip-range-list-in-python/
+    """
+
+    ip_range = []
+
+    if (ipaddress.ip_address(start_ip).version == 6) or (ipaddress.ip_address(end_ip).version == 6):
+        print("IPv6 IP range not supported in this function: {} - {}".format(start_ip, end_ip))
+        return ip_range
+
+    start = list(map(int, start_ip.split(".")))
+    end = list(map(int, end_ip.split(".")))
+    temp = start
+
+    ip_range.append(start_ip)
+    while temp != end:
+        start[3] += 1
+        for i in (3, 2, 1):
+            if temp[i] == 256:
+                temp[i] = 0
+                temp[i - 1] += 1
+        ip_range.append(".".join(map(str, temp)))
+
+    return ip_range
+
+
+def http_status_code(http_code):
+    """Contains a database of all known HTTP status codes and their corresponding plain text description.  For use in
+    both program output as well as parsing for specific issue types.
+
+    Args:
+        http_code (int): A number containing the HTTP status code to lookup
+
+    Returns:
+        string: Returns a description of the status code.
+    """
+
+    http_codes = {
+        200: "OK",
+        201: "OK: Created",
+        202: "OK: Accepted",
+        203: "OK: Non-Authoritative Information",
+        204: "OK: No Content",
+        205: "OK: Reset Content",
+        206: "OK: Partial Content",
+        207: "OK: Multi-Status",
+        208: "OK: Already Reported",
+        226: "OK: IM Used",
+        300: "Redirected: Multiple Choices",
+        301: "Redirected: Moved Permanently",
+        302: "Redirected: Found",
+        303: "Redirected: See Other",
+        304: "Redirected: Not Modified",
+        305: "Redirected: Use Proxy",
+        306: "Redirected: Switch Proxy",
+        307: "Redirected: Temporary Redirect",
+        308: "Redirected: Permanent Redirect",
+        400: "Client Error: Bad Request",
+        401: "Client Error: Unauthorized",
+        402: "Client Error: Payment Required",
+        403: "Client Error: Forbidden",
+        404: "Client Error: Not Found",
+        405: "Client Error: Method Not Allowed",
+        406: "Client Error: Not Acceptable",
+        407: "Client Error: Proxy Authentication Required",
+        408: "Client Error: Request Timeout",
+        409: "Client Error: Conflict",
+        410: "Client Error: Gone",
+        411: "Client Error: Length Required",
+        412: "Client Error: Precondition Failled",
+        413: "Client Error: Payload Too Large",
+        414: "Client Error: URI Too Large",
+        415: "Client Error: Unsupported Media Type",
+        416: "Client Error: Range Not Satisfiable",
+        417: "Client Error: Expectation Failed",
+        418: "Client Error: I'm a teapot",
+        421: "Client Error: Misdirected Request",
+        422: "Client Error: Un-processable Entity",
+        423: "Client Error: Locked",
+        424: "Client Error: Failed Dependency",
+        426: "Client Error: Upgrade Required",
+        428: "Client Error: Precondition Required",
+        429: "Client Error: Too Many Requests",
+        431: "Client Error: Request Header Fields Too Large",
+        440: "Client Error: Login Time-Out",
+        444: "Client Error: No Response",
+        449: "Client Error: Retry With",
+        451: "Client Error: Unavailable For Legal Reasons",
+        495: "Client Error: SSL Certificate Error",
+        496: "Client Error: SSL Certificate Required",
+        497: "Client Error: HTTP Request Sent to HTTPS Port",
+        499: "Client Error: Client Closed Request",
+        500: "Server Error: Internal Server Error",
+        501: "Server Error: Not Implemented",
+        502: "Server Error: Bad Gateway",
+        503: "Server Error: Service Unavailable",
+        504: "Server Error: Gateway Timeout",
+        505: "Server Error: HTTP Version Not Supported",
+        507: "Server Error: Insufficient Storage",
+        508: "Server Error: Loop Detected",
+        510: "Server Error: Not Extended",
+        511: "Server Error: Network Authentication Required",
+        520: "Server Error: Unknown Error when connecting to server behind load balancer",
+        521: "Server Error: Web Server behind load balancer is down",
+        522: "Server Error: Connection Timed Out to server behind load balancer",
+        523: "Server Error: Server behind load balancer is unreachable",
+        524: "Server Error: TCP handshake with server behind load balancer completed but timed out",
+        525: "Server Error: Load balancer could not negotiate a SSL/TLS handshake with server behind load balancer",
+        526: "Server Error: Server behind load balancer returned invalid SSL/TLS cert to load balancer",
+        527: "Server Error: Load balancer request timed out/failed after WAN connection was established to origin server",
+    }
+
+    http_status = http_codes.get(http_code, "NA")
+
+    return http_status
